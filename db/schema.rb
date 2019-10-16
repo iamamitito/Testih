c6849f2709b83e67a7b2eae93358579ccae9020b
Héctor Espinoza <hespinoza@uchile.cl>
2019-10-15 21:08:43 -0300

    tests created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_000524) do
+ActiveRecord::Schema.define(version: 2019_10_16_000750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tests", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_10_16_000524) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tests", "users"
 end
