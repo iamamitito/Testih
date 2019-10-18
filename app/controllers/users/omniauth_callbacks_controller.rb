7e483364beebd8a640577d4ba666284ff1874b1a
Héctor Espinoza <hespinoza@uchile.cl>
2019-10-18 12:38:07 -0300

    fb sign up working in development

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+    def facebook
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        if @user.persisted?
+          sign_in_and_redirect @user
+          set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        else
+            redirect_to root_path, notice: 'Error al iniciar sesión con Facebook.'
+        end
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'home/home_index'
   get 'home/map'
   resources :tests
-  devise_for :users
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root to: 'home#home_index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
