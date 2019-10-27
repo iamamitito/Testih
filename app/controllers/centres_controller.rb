e47cfb5516bd42128b48c2d8a8f1e8d1295d58a9
Héctor Espinoza <hespinoza@uchile.cl>
2019-10-27 00:43:53 -0300

    markers changed and backgrounds added

--- app/controllers/centres_controller.rb
+++ app/controllers/centres_controller.rb
@@ -5,6 +5,12 @@ class CentresController < ApplicationController
     @hash = Gmaps4rails.build_markers(@centres) do |centre, marker|
       marker.lat centre.latitude
       marker.lng centre.longitude
+      marker.picture({
+                  url: "https://i.imgur.com/O6W2WqR.png?1",
+                  width: 30,
+                  height: 30
+                 })
+      marker.infowindow centre.name
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
   has_many :tests, dependent: :destroy
+  
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
